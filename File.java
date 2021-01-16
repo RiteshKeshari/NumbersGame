d51365f5d9fa2f1e874b8734b375e764513f7c3f
RiteshKeshari <77539323+RiteshKeshari@users.noreply.github.com>
2021-01-16 22:16:46 +0530

    java file created

---
+++ File.java
@@ -0,0 +1,5 @@
+# NumbersGame 
+This game has been made by me 
+it generate the random no. and we need to guess the generated no. 
+every time it generate different no , no-one knows the no even me who created this :)
+you will be provided the chance to guess the no , exactly five times after which winning or losing would be decided 
